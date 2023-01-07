25e4f79ad89039dd6ec30b3876e95deb12fd81db
Sayma Shaikh <sayma.shaikh@BBD.INTL>
2023-01-07 12:09:07 +0530

    Initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogListComponent } from './components/blog-list/blog-list.component';
+import { CreateBlogComponent } from './components/create-blog/create-blog.component';
+import { EditBlogComponent } from './components/edit-blog/edit-blog.component';
+import { BlogDetailComponent } from './components/blog-detail/blog-detail.component';
+import { RegistrationComponent } from './authentication/registration/registration.component';
+import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogListComponent,
+    CreateBlogComponent,
+    EditBlogComponent,
+    BlogDetailComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
